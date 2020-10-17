367f20d7440f8fe80d26479706dc7cdb7313777b
Kévin Dunglas <dunglas@gmail.com>
2020-10-17 23:02:57 +0200

    ci: set up GitHub Actions. Add a test for x-smaxage

--- httpcache_test.go
+++ httpcache_test.go
@@ -32,6 +32,32 @@ func TestMaxAge(t *testing.T) {
 	}
 }
 
+func TestSMaxAge(t *testing.T) {
+	tester := caddytest.NewTester(t)
+	tester.InitServer(` 
+	{
+	  http_port     9080
+	  https_port    9443
+	}
+	localhost:9080 {
+		route /cache-s-maxage {
+			cache
+			header Cache-Control "s-maxage=5"
+			respond "Hello, s-maxage!"
+		}
+	}`, "caddyfile")
+
+	resp1, _ := tester.AssertGetResponse(`http://localhost:9080/cache-s-maxage`, 200, "Hello, s-maxage!")
+	if resp1.Header.Get("Cache-Status") != "Caddy; fwd=uri-miss; stored" {
+		t.Errorf("unexpected Cache-Status header %v", resp1.Header.Get("Cache-Status"))
+	}
+
+	resp2, _ := tester.AssertGetResponse(`http://localhost:9080/cache-s-maxage`, 200, "Hello, s-maxage!")
+	if resp2.Header.Get("Cache-Status") != "Caddy; hit" {
+		t.Errorf("unexpected Cache-Status header %v", resp2.Header.Get("Cache-Status"))
+	}
+}
+
 func TestOlricConfig(t *testing.T) {
 	tester := caddytest.NewTester(t)
 	tester.InitServer(` 

6f6bc30b54ab0aaed6c63e0c5bf1c3933dd6ca42
darkweak <darkweak@protonmail.com>
2023-11-30 12:08:22 +0100

    feat(dependencies): bump to Souin v1.6.44

--- app.go
+++ app.go
@@ -3,7 +3,7 @@ package httpcache
 import (
 	"errors"
 
-	"github.com/caddyserver/caddy/v2"
+	caddy "github.com/caddyserver/caddy/v2"
 	"github.com/darkweak/souin/configurationtypes"
 	"github.com/darkweak/souin/pkg/storage"
 	"github.com/darkweak/souin/pkg/surrogate/providers"
@@ -11,9 +11,9 @@ import (
 
 // SouinApp contains the whole Souin necessary items
 type SouinApp struct {
-	*DefaultCache
+	DefaultCache
 	// The provider to use.
-	Storer storage.Storer
+	Storers []storage.Storer
 	// Surrogate storage to support th econfiguration reload without surrogate-key data loss.
 	SurrogateStorage providers.SurrogateInterface
 	// Cache-key tweaking.
@@ -29,7 +29,7 @@ func init() {
 }
 
 // Provision implements caddy.Provisioner
-func (s *SouinApp) Provision(_ caddy.Context) error {
+func (s SouinApp) Provision(_ caddy.Context) error {
 	return nil
 }
 
@@ -37,7 +37,7 @@ func (s *SouinApp) Provision(_ caddy.Context) error {
 func (s SouinApp) Start() error {
 	_, _ = up.Delete(stored_providers_key)
 	_, _ = up.LoadOrStore(stored_providers_key, newStorageProvider())
-	if s.DefaultCache != nil && s.DefaultCache.GetTTL() == 0 {
+	if s.DefaultCache.GetTTL() == 0 {
 		return errors.New("Invalid/Incomplete default cache declaration")
 	}
 	return nil
--- httpcache.go
+++ httpcache.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/buraksezer/olric/config"
-	"github.com/caddyserver/caddy/v2"
+	caddy "github.com/caddyserver/caddy/v2"
 	"github.com/caddyserver/caddy/v2/caddyconfig"
 	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
 	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
@@ -34,8 +34,8 @@ func init() {
 type SouinCaddyMiddleware struct {
 	*middleware.SouinBaseHandler
 	logger        *zap.Logger
-	Configuration *Configuration
 	cacheKeys     configurationtypes.CacheKeys
+	Configuration Configuration
 	// Logger level, fallback on caddy's one when not redefined.
 	LogLevel string `json:"log_level,omitempty"`
 	// Allowed HTTP verbs to be cached by the system.
@@ -62,6 +62,8 @@ type SouinCaddyMiddleware struct {
 	TTL configurationtypes.Duration `json:"ttl,omitempty"`
 	// Time to live for a stale key, using time.duration.
 	Stale configurationtypes.Duration `json:"stale,omitempty"`
+	// Storage providers chaining and order.
+	Storers []string `json:"storers,omitempty"`
 	// The default Cache-Control header value if none set by the upstream server.
 	DefaultCacheControl string `json:"default_cache_control,omitempty"`
 	// The cache name to use in the Cache-Status response header.
@@ -84,51 +86,49 @@ func (s *SouinCaddyMiddleware) ServeHTTP(rw http.ResponseWriter, r *http.Request
 }
 
 func (s *SouinCaddyMiddleware) configurationPropertyMapper() error {
-	if s.Configuration != nil {
-		return nil
-	}
-	defaultCache := &DefaultCache{
-		Badger:              s.Badger,
-		Nuts:                s.Nuts,
-		Key:                 s.Key,
-		DefaultCacheControl: s.DefaultCacheControl,
-		CacheName:           s.CacheName,
-		Distributed:         s.Olric.URL != "" || s.Olric.Path != "" || s.Olric.Configuration != nil || s.Etcd.Configuration != nil || s.Redis.URL != "" || s.Redis.Configuration != nil,
-		Headers:             s.Headers,
-		Olric:               s.Olric,
-		Etcd:                s.Etcd,
-		Redis:               s.Redis,
-		Timeout:             s.Timeout,
-		TTL:                 s.TTL,
-		Stale:               s.Stale,
-	}
-	if s.Configuration == nil {
-		s.Configuration = &Configuration{
+	if s.Configuration.GetDefaultCache() == nil {
+		defaultCache := DefaultCache{
+			Badger:              s.Badger,
+			Nuts:                s.Nuts,
+			Key:                 s.Key,
+			DefaultCacheControl: s.DefaultCacheControl,
+			CacheName:           s.CacheName,
+			Distributed:         s.Olric.URL != "" || s.Olric.Path != "" || s.Olric.Configuration != nil || s.Etcd.Configuration != nil || s.Redis.URL != "" || s.Redis.Configuration != nil,
+			Headers:             s.Headers,
+			Olric:               s.Olric,
+			Etcd:                s.Etcd,
+			Redis:               s.Redis,
+			Timeout:             s.Timeout,
+			TTL:                 s.TTL,
+			Stale:               s.Stale,
+			Storers:             s.Storers,
+		}
+		s.Configuration = Configuration{
 			CacheKeys:    s.cacheKeys,
 			DefaultCache: defaultCache,
 			LogLevel:     s.LogLevel,
 		}
 	}
-	s.Configuration.DefaultCache = defaultCache
+
 	return nil
 }
 
 // FromApp to initialize configuration from App structure.
 func (s *SouinCaddyMiddleware) FromApp(app *SouinApp) error {
-	if s.Configuration == nil {
-		s.Configuration = &Configuration{
+	if s.Configuration.GetDefaultCache() == nil {
+		s.Configuration = Configuration{
 			URLs: make(map[string]configurationtypes.URL),
 		}
 	}
 
-	if app.DefaultCache == nil {
+	if app.DefaultCache.GetTTL() == 0 {
 		return nil
 	}
 
 	s.Configuration.API = app.API
 
-	if s.Configuration.DefaultCache == nil {
-		s.Configuration.DefaultCache = &DefaultCache{
+	if s.Configuration.GetDefaultCache() == nil {
+		s.Configuration.DefaultCache = DefaultCache{
 			AllowedHTTPVerbs:    app.DefaultCache.AllowedHTTPVerbs,
 			Headers:             app.Headers,
 			Key:                 app.Key,
@@ -171,6 +171,9 @@ func (s *SouinCaddyMiddleware) FromApp(app *SouinApp) error {
 	}
 	if dc.Stale.Duration == 0 {
 		s.Configuration.DefaultCache.Stale = appDc.Stale
+	}
+	if len(dc.Storers) == 0 {
+		s.Configuration.DefaultCache.Storers = appDc.Storers
 	}
 	if dc.Timeout.Backend.Duration == 0 {
 		s.Configuration.DefaultCache.Timeout.Backend = appDc.Timeout.Backend
@@ -228,7 +231,7 @@ func (s *SouinCaddyMiddleware) Provision(ctx caddy.Context) error {
 		return err
 	}
 
-	bh := middleware.NewHTTPCacheHandler(s.Configuration)
+	bh := middleware.NewHTTPCacheHandler(&s.Configuration)
 	surrogates, ok := up.LoadOrStore(surrogate_key, bh.SurrogateKeyStorer)
 	if ok {
 		bh.SurrogateKeyStorer = surrogates.(surrogates_providers.SurrogateInterface)
@@ -237,41 +240,43 @@ func (s *SouinCaddyMiddleware) Provision(ctx caddy.Context) error {
 	s.SouinBaseHandler = bh
 	dc := s.SouinBaseHandler.Configuration.GetDefaultCache()
 	if dc.GetDistributed() {
-		if eo, ok := s.SouinBaseHandler.Storer.(*storage.EmbeddedOlric); ok {
-			name := fmt.Sprintf("0.0.0.0:%d", config.DefaultPort)
-			if dc.GetOlric().Configuration != nil {
-				oc := dc.GetOlric().Configuration.(*config.Config)
-				name = fmt.Sprintf("%s:%d", oc.BindAddr, oc.BindPort)
-			} else if dc.GetOlric().Path != "" {
-				name = dc.GetOlric().Path
-			}
-
-			key := "Embedded-" + name
-			v, _ := up.LoadOrStore(stored_providers_key, newStorageProvider())
-			v.(*storage_providers).Add(key)
-
-			if eo.GetDM() == nil {
-				v, l, e := up.LoadOrNew(key, func() (caddy.Destructor, error) {
-					s.logger.Sugar().Debug("Create a new olric instance.")
-					eo, err := storage.EmbeddedOlricConnectionFactory(s.Configuration)
-					if eo != nil {
-						return eo.(*storage.EmbeddedOlric), err
-					}
-					return nil, err
-				})
-
-				if l && e == nil {
-					s.SouinBaseHandler.Storer = v.(storage.Storer)
+		for _, currentStorer := range s.SouinBaseHandler.Storers {
+			if eo, ok := currentStorer.(*storage.EmbeddedOlric); ok {
+				name := fmt.Sprintf("0.0.0.0:%d", config.DefaultPort)
+				if dc.GetOlric().Configuration != nil {
+					oc := dc.GetOlric().Configuration.(*config.Config)
+					name = fmt.Sprintf("%s:%d", oc.BindAddr, oc.BindPort)
+				} else if dc.GetOlric().Path != "" {
+					name = dc.GetOlric().Path
 				}
-			} else {
-				s.logger.Sugar().Debug("Store the olric instance.")
-				_, _ = up.LoadOrStore(key, s.SouinBaseHandler.SurrogateKeyStorer)
+
+				key := "Embedded-" + name
+				v, _ := up.LoadOrStore(stored_providers_key, newStorageProvider())
+				v.(*storage_providers).Add(key)
+
+				if eo.GetDM() == nil {
+					v, l, e := up.LoadOrNew(key, func() (caddy.Destructor, error) {
+						s.logger.Sugar().Debug("Create a new olric instance.")
+						eo, err := storage.EmbeddedOlricConnectionFactory(&s.Configuration)
+						if eo != nil {
+							return eo.(*storage.EmbeddedOlric), err
+						}
+						return nil, err
+					})
+
+					if l && e == nil {
+						s.SouinBaseHandler.Storers = append(s.SouinBaseHandler.Storers, v.(storage.Storer))
+					}
+				} else {
+					s.logger.Sugar().Debug("Store the olric instance.")
+					_, _ = up.LoadOrStore(key, s.SouinBaseHandler.SurrogateKeyStorer)
+				}
 			}
 		}
 	}
 
-	if app.Storer == (storage.Storer)(nil) {
-		app.Storer = s.SouinBaseHandler.Storer
+	if len(app.Storers) == 0 {
+		app.Storers = s.SouinBaseHandler.Storers
 	}
 
 	if app.SurrogateStorage == (surrogates_providers.SurrogateInterface)(nil) {
@@ -285,8 +290,8 @@ func (s *SouinCaddyMiddleware) Provision(ctx caddy.Context) error {
 
 func parseCaddyfileGlobalOption(h *caddyfile.Dispenser, _ interface{}) (interface{}, error) {
 	souinApp := new(SouinApp)
-	cfg := &Configuration{
-		DefaultCache: &DefaultCache{
+	cfg := Configuration{
+		DefaultCache: DefaultCache{
 			AllowedHTTPVerbs: make([]string, 0),
 			Distributed:      false,
 			Headers:          []string{},
@@ -299,7 +304,7 @@ func parseCaddyfileGlobalOption(h *caddyfile.Dispenser, _ interface{}) (interfac
 		URLs: make(map[string]configurationtypes.URL),
 	}
 
-	err := parseConfiguration(cfg, h, true)
+	err := parseConfiguration(&cfg, h, true)
 
 	souinApp.DefaultCache = cfg.DefaultCache
 	souinApp.API = cfg.API
@@ -319,10 +324,11 @@ func (s *SouinCaddyMiddleware) UnmarshalCaddyfile(h *caddyfile.Dispenser) error 
 	dc := DefaultCache{
 		AllowedHTTPVerbs: make([]string, 0),
 	}
-	s.Configuration = &Configuration{
-		DefaultCache: &dc,
+	s.Configuration = Configuration{
+		DefaultCache: dc,
 	}
-	return parseConfiguration(s.Configuration, h, false)
+
+	return parseConfiguration(&s.Configuration, h, false)
 }
 
 // Interface guards
